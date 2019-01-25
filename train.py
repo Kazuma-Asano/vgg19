758ed0e199fadd1f9f51ef2f73f729f4ddec19bf
浅野一真 <kazuma@asanokazumanoMacBook-Pro.local>
2019-01-25 11:30:52 +0900

    train.py update

--- train.py
+++ train.py
@@ -1,12 +1,33 @@
-
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import argparse
 import os
 import torch
+import torch.nn as nn
 
 from dataloder import dataloader
 from network import VGG19, initialize_weights
+from util import progress_bar
+
+def get_parser():
+    # Training settings
+    parser = argparse.ArgumentParser(description='Pytorch vgg19')
+    parser.add_argument('--batchSize', '-b', type=int, default=16, help='training batch size')
+    parser.add_argument('--testBatchSize', '-tb', type=int, default=8, help='testing batch size')
+    parser.add_argument('--nEpochs', '-e', type=int, default=100, help='number of epochs to train for')
+    parser.add_argument('--lr', type=float, default=0.001, help='Learning Rate. Default=0.001')
+    parser.add_argument('--cuda', action='store_true', help='use cuda?') # GPUを利用するなら $python train.py --cuda
+    parser.add_argument('--threads', type=int, default=4, help='number of threads for data loader to use')
+    parser.add_argument('--seed', type=int, default=123, help='random seed to use. Default=123')
+    parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
+
+    option = parser.parse_args()
+    print(option)
+
+    return option
 
 ####### TRAIN #########
-def train(model, criterion, optimizer, scheduler):
+def train(model, criterion, optimizer, scheduler=None):
     model.train() #drop outを適用
     train_loss = 0
     correct = 0
@@ -74,9 +95,26 @@ if __name__ == '__main__':
     #####################
     #### dataの読み込み ###
     #####################
+    opt = get_parser()
+
+    if opt.cuda and not torch.cuda.is_available():
+        raise Exception("No GPU found, please run without --cuda")
+
+    gpu_ids = []
+    torch.manual_seed(opt.seed)
+
+    if opt.cuda:
+        torch.cuda.manual_seed(opt.seed)
+        gpu_ids = [0]
+
+    #####################
+    ## datasetの読み込み ##
+    #####################
     print('==> Loading data...')
     print('-' * 20)
-    trainloader, testloader, class_names = dataloader(batch_size=4)
+    trainloader, testloader, class_names = dataloader(batch_size=opt.batchSize,
+                                                      tbatchSize=opt.tbatchSize,
+                                                      threads=opt.threads)
     print('OK')
 
     #####################
@@ -88,4 +126,10 @@ if __name__ == '__main__':
     model.apply(initialize_weights) # 初期化
     print(model)
     print('OK')
-    
+
+    # optimaizerなどの設定
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+
+    for epoch in range(1, opt.nEpochs):
+        train(model, criterion, optimizer)
